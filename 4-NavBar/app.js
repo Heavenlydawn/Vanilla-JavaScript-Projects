3450978f5a3d7cc22e91a9881a0801d735e02a9a
Heavenlydawn <gabrielheavenlydawn@gmail.com>
2023-02-17 20:54:35 +0100

    Sidebar project completed

--- 4-NavBar/app.js
+++ 4-NavBar/app.js
@@ -6,33 +6,23 @@
 
 // Methods used - (classList add remove contains toggle)
 
-
-
-const navToggle = document.querySelector(".nav-toggle"); 
+const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
-navToggle.addEventListener("click", function(){
+navToggle.addEventListener("click", function () {
+  //  console.log(links.classList)
+  // console.log(links.classList.contains("links")) - result - true
+  // console.log(links.classList.contains("random")) - result - false
 
-    //  console.log(links.classList)
-    // console.log(links.classList.contains("links")) - result - true
-    // console.log(links.classList.contains("random")) - result - false
+  // The classList property returns or shows all the classes the element has
+  // The contains method shows a particular class the element has
 
-    // The classList property returns or shows all the classes the element has
-    // The contains method shows a particular class the element has
-    
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  } else {
+    links.classList.add("show-links");
+  }
 
-
-    if(links.classList.contains("show-links")){
-        links.classList.remove("show-links")
-    }else{
-        links.classList.add("show-links")
-    }
-
-
-    // The toggle method allows us to use just one line of code - it takes a class and returns either the add or remove method
-    // links.classList.toggle("show-links")
+  // The toggle method allows us to use just one line of code - it takes a class and returns either the add or remove method
+  // links.classList.toggle("show-links")
 });
- 
-
-
-
